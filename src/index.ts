ea006e60caca871e4b484f209f35b17f9054f6c3
Yousef Mahmoud <usef.mahmoud@outlook.com>
2026-05-01 18:19:23 +0300

    fix: add missing semicolons in example code for consistency

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,24 @@ import { Lexer } from './lexer/lexer';
 import { TokenType } from './lexer/tokens';
 
 const lexer = new Lexer(`
-name = "yousef"
-age = 30
+name = "yousef";
+age = 30;
 
 fun greet(name) {
-  print("Hello, " + name + "!")
-  return "done"
+  print("Hello, " + name + "!");
+  return "done";
 }
 
-greet(name)
+greet(name);
 `);
 
 try {
   const tokens = lexer.tokenize();
   console.log(
-    tokens.map((token) => ({ token: TokenType[token.type], value: token.value }))
+    tokens.map((token) => ({
+      token: TokenType[token.type],
+      value: token.value,
+    }))
   );
 } catch (error) {
   console.error('Lexer error:', error);
