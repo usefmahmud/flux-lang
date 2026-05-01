b5159e16d657c727b2cf83dff84fcafe9500f2bd
Yousef Mahmoud <usef.mahmoud@outlook.com>
2026-05-01 14:49:04 +0300

    feat: implement Lexer functionality and restructure token definitions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('أهلا بالعالم');
+import { Lexer } from './lexer/lexer';
+import { TokenType } from './lexer/tokens';
+
+const lexer = new Lexer(`
+name = "yousef"
+age = 30
+
+fun greet(name) {
+  print("Hello, " + name + "!")
+  return "done"
+}
+
+greet(name)
+`);
+
+try {
+  const tokens = lexer.tokenize();
+  console.log(
+    tokens.map((token) => ({ token: TokenType[token.type], value: token.value }))
+  );
+} catch (error) {
+  console.error('Lexer error:', error);
+}
